9baab99ef4768c8723b646452a2ccac9052e6d62
Saksham-Bansal-3110 <sakshambansal3110@gmail.com>
2025-08-23 14:14:00 +0000

    Added functionality for epubs too

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import sys
+from ebooklib import epub, ITEM_DOCUMENT
+from bs4 import BeautifulSoup
+import os
 from stats import num_words 
 from stats import char_count
 from stats import char_report
 
 def get_book_text(filepath: str) -> str:
-    with open(filepath) as f:
-        return f.read()
+    
+    if filepath.endswith(".txt"):
+        with open(filepath) as f:
+            return f.read()
+        
+    elif filepath.endswith(".epub"):
+        book = epub.read_epub(filepath)
+        text = []
+        for item in book.get_items():
+            if item.get_type() == ITEM_DOCUMENT:
+                soup = BeautifulSoup(item.get_body_content(),"html.parser")
+                text.append(soup.get_text())
+        return "\n".join(text)
+    
+    else:
+        raise ValueError("Unsupported file type. Use .txt or .epub")
     
 def main():
+    
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
